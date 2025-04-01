f753c3202e19b088940c5fa81a8d36b885d6bce0
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-01 16:13:36 -0400

    Tests for vendor#menu

--- rails/app/models/vendor.rb
+++ rails/app/models/vendor.rb
@@ -2,5 +2,7 @@
 
 class Vendor < ApplicationRecord
   belongs_to :track
+  has_many :menu_items, dependent: :destroy
+
   validates :name, presence: true
 end
--- rails/spec/models/vendor_spec.rb
+++ rails/spec/models/vendor_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Vendor do
+  subject(:vendor) { build(:vendor) }
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
@@ -10,5 +12,23 @@ RSpec.describe Vendor do
   describe 'associations' do
     it { is_expected.to belong_to(:track) }
     it { is_expected.to have_many(:menu_items).dependent(:destroy) }
+  end
+
+  describe '#menu' do
+    context 'when the menu is empty' do
+      it { expect(vendor.menu).to be_empty }
+    end
+
+    context 'when the menu has items without a category' do
+      let(:menu_item) { build(:menu_item, vendor:, category: nil) }
+
+      it 'returns the menu items' do
+        expect(vendor.menu).to \
+          equal({ '*' => [
+                  { name: menu_item.name, description: menu_item.description,
+                    price: menu_item.price }
+                ] })
+      end
+    end
   end
 end

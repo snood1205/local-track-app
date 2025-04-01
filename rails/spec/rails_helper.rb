fc1bf159b14768dd70868b5201e3f56672693479
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-01 13:09:16 -0400

    Simplify rails app

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -23,9 +23,7 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
-  ]
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!

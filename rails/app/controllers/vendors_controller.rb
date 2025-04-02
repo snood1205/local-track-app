823fa3fc4d7534f8736b4c4c8120795d4070959c
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-02 09:00:21 -0400

    Skeleton for /vendors/grouped

--- rails/app/controllers/vendors_controller.rb
+++ rails/app/controllers/vendors_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class VendorsController < ApplicationController
+  def index
+    @vendors = Vendor.all
+    render json: @vendors.as_json(only: %i[id name category])
+  end
+
+  def grouped
+  end
 end
--- rails/spec/requests/vendors_spec.rb
+++ rails/spec/requests/vendors_spec.rb
@@ -3,21 +3,52 @@
 require 'rails_helper'
 
 RSpec.describe 'Vendors' do
-  describe 'GET /index' do
-    let!(:beverage_vendor) { create(:vendor, name: 'Beverage Vendor', category: 'Food & Drinks') }
-    let!(:food_vendor) { create(:vendor, name: 'Food Vendor', category: 'Food & Drinks') }
-    let!(:merch_vendor) { create(:vendor, name: 'Merch Vendor', category: 'Clothing') }
+  let!(:beverage_vendor) { create(:vendor, name: 'Beverage Vendor', category: 'Food & Drinks') }
+  let!(:food_vendor) { create(:vendor, name: 'Food Vendor', category: 'Food & Drinks') }
+  let!(:merch_vendor) { create(:vendor, name: 'Merch Vendor', category: 'Clothing') }
 
+  describe 'GET /index' do
     before { get '/vendors' }
 
     it { expect(response).to have_http_status(:ok) }
     it { expect(response.parsed_body).to be_a(Array) }
     it { expect(response.parsed_body.size).to eq(3) }
 
-    %w[name category].each do |attribute|
+    %w[id name category].each do |attribute|
       it { expect(response.parsed_body[0][attribute]).to eq(beverage_vendor.send(attribute)) }
       it { expect(response.parsed_body[1][attribute]).to eq(food_vendor.send(attribute)) }
       it { expect(response.parsed_body[2][attribute]).to eq(merch_vendor.send(attribute)) }
     end
+
+    it { expect(response.parsed_body[0]).not_to have_key('trackId') }
+    it { expect(response.parsed_body[0]).not_to have_key('createdAt') }
+    it { expect(response.parsed_body[0]).not_to have_key('updatedAt') }
+  end
+
+  describe 'GET /grouped' do
+    before { get '/vendors/grouped' }
+
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(response.parsed_body).to be_a(Hash) }
+
+    it { expect(response.parsed_body.size).to eq(2) }
+
+    it { expect(response.parsed_body['Food & Drinks']).to be_a(Array) }
+    it { expect(response.parsed_body['Food & Drinks'].size).to eq(2) }
+
+    it { expect(response.parsed_body['Food & Drinks'][0]['id']).to eq(beverage_vendor.id) }
+    it { expect(response.parsed_body['Food & Drinks'][0]['name']).to eq(beverage_vendor.name) }
+    it { expect(response.parsed_body['Food & Drinks'][1]['id']).to eq(food_vendor.id) }
+    it { expect(response.parsed_body['Food & Drinks'][1]['name']).to eq(food_vendor.name) }
+
+    it { expect(response.parsed_body['Clothing']).to be_a(Array) }
+    it { expect(response.parsed_body['Clothing'].size).to eq(1) }
+    it { expect(response.parsed_body['Clothing'][0]['id']).to eq(merch_vendor.id) }
+    it { expect(response.parsed_body['Clothing'][0]['name']).to eq(merch_vendor.name) }
+
+    it { expect(response.parsed_body['Food & Drinks'][0]).not_to have_key('category') }
+    it { expect(response.parsed_body['Food & Drinks'][0]).not_to have_key('trackId') }
+    it { expect(response.parsed_body['Food & Drinks'][0]).not_to have_key('createdAt') }
+    it { expect(response.parsed_body['Food & Drinks'][0]).not_to have_key('updatedAt') }
   end
 end

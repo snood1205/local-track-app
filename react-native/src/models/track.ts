75114f024b90cb16d6583153e830db6157154cc3
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-02 09:58:36 -0400

    Start work on models. Create specs to not expose ids

--- react-native/src/models/track.ts
+++ react-native/src/models/track.ts
@@ -1,6 +1,10 @@
+import { Address } from "./address";
+
 export interface Track {
   name: string;
   length: string; // stored as decimal in psql, so kept a string for precision
-  length_unit: "mi" | "km" | "yd" | "ft" | "m";
-  human_readable_length: string; // e.g. "3.1 miles"
+  lengthUnit: "mi" | "km" | "yd" | "ft" | "m";
+  material: string;
+  humanReadableLength: string; // e.g. "3.1 miles"
+  address?: Address;
 }
